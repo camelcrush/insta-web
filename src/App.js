bf35d1ecc58396c9716a569b56cd27ff46e9b9c6
camelcrush <dkxm0927@naver.com>
2022-04-30 18:49:16 +0900

    Router Setup: BrowserRouter, Route, Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFount from "./screens/NotFound";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = true;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFount />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/screens/NotFound.js
@@ -0,0 +1,5 @@
+const NotFount = () => {
+  return <h1>Not found.</h1>;
+};
+
+export default NotFount;
