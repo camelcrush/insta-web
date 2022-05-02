eff16abcb8d5a3636f377906c6203ad072138b42
camelcrush <dkxm0927@naver.com>
2022-05-02 22:16:28 +0900

    React Hook Form

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import PropTypes from "prop-types";
 
 const PageTitle = ({ title }) => {
   return (
@@ -6,6 +7,10 @@ const PageTitle = ({ title }) => {
       <title>{title} | Instaclone</title>
     </Helmet>
   );
+};
+
+PageTitle.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default PageTitle;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,11 +3,13 @@ import {
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Seperator from "../components/auth/Seperator";
 import PageTitle from "../components/PageTitle";
@@ -22,6 +24,19 @@ const FacebookLogin = styled.div`
 `;
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: "onChange" });
+  const onSubmitValid = (data) => {
+    // Valid일 때
+    // console.log(data);
+  };
+  const onSubmitInValid = (data) => {
+    // InValid일 때
+    // console.log(data, "Invalid");
+  };
   return (
     <AuthLayout>
       <PageTitle title="Log in" />
@@ -29,10 +44,32 @@ const Login = () => {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        <form>
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Log in" />
+        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInValid)}>
+          <Input
+            {...register("username", {
+              required: "Username is required.", // required: true || errorMessage 가능
+              minLength: {
+                value: 5,
+                message: "Username should be longer than 5 chars.",
+              },
+            })}
+            name="username"
+            type="text"
+            placeholder="Username"
+            // hasError prop
+            hasError={Boolean(errors?.username?.message)}
+          />
+          <FormError message={errors?.username?.message} />
+          <Input
+            {...register("password", { required: "Password is required." })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            hasError={Boolean(errors?.password?.message)}
+          />
+          <FormError message={errors?.password?.message} />
+
+          <Button type="submit" value="Log in" disabled={!isValid} />
         </form>
         <Seperator />
         <FacebookLogin>
