0ad6bc3b98ca42033c4fd55f7d4d88e906476c90
camelcrush <dkxm0927@naver.com>
2022-05-03 23:29:43 +0900

    Dark Mode

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 import routes from "./routes";
 
-const TOKEN = "token";
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE";
 
 // 페이지가 새로고침될 때마다 local storage에 token 유무 확인, 있으면 true 없으면 false
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
@@ -19,7 +20,18 @@ export const logUserOut = (navigate) => {
   // or window.location.reload()
 };
 
-export const darkModeVar = makeVar(false);
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
+
+export const enableDarkMode = () => {
+  localStorage.setItem(DARK_MODE, "enabled");
+  darkModeVar(true);
+};
+
+export const disableDarkMode = () => {
+  localStorage.removeItem(DARK_MODE);
+  darkModeVar(false);
+};
+
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
