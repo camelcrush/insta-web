b6d125051669ca2baab60256304f6119c4a989cf
camelcrush <dkxm0927@naver.com>
2022-05-04 21:14:11 +0900

    header 2: login btn in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
+import { useReactiveVar } from "@apollo/client";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faCompass, faUser } from "@fortawesome/free-regular-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { isLoggedInVar } from "../apollo";
+import routes from "../routes";
 
 const SHeader = styled.header`
   display: flex;
@@ -28,7 +32,16 @@ const Icon = styled.span`
   margin-left: 15px;
 `;
 
+const Button = styled.span`
+  background-color: ${(props) => props.theme.accent};
+  border-radius: 4px;
+  padding: 4px 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 const Header = () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <SHeader>
       <Wrapper>
@@ -36,15 +49,23 @@ const Header = () => {
           <FontAwesomeIcon icon={faInstagram} size="2x" />
         </Column>
         <Column>
-          <Icon>
-            <FontAwesomeIcon icon={faHome} size="lg" />
-          </Icon>
-          <Icon>
-            <FontAwesomeIcon icon={faCompass} size="lg" />
-          </Icon>
-          <Icon>
-            <FontAwesomeIcon icon={faUser} size="lg" />
-          </Icon>
+          {isLoggedIn ? (
+            <>
+              <Icon>
+                <FontAwesomeIcon icon={faHome} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faCompass} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </Icon>
+            </>
+          ) : (
+            <Link to={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
         </Column>
       </Wrapper>
     </SHeader>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,9 @@ import Header from "./Header";
 
 const Content = styled.main`
   margin: 0 auto;
+  margin-top: 45px;
+  max-width: 930px;
+  width: 100%;
 `;
 
 const Layout = ({ children }) => {
