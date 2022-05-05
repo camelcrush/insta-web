69ca862976a4685077124a21c340cb9e59ebfa57
camelcrush <dkxm0927@naver.com>
2022-05-05 12:36:20 +0900

    Clean Photo Component

---
+++ src/components/feed/Photo.js
@@ -0,0 +1,108 @@
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Avatar from "../Avatar";
+import { FatText } from "../shared";
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.boderColor};
+  border-radius: 4px;
+  margin-bottom: 60px;
+  max-width: 615px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(239, 239, 239);
+`;
+
+const Username = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 12px 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
+`;
+
+function Photo({ id, user, file, isLiked, likes }) {
+  return (
+    <PhotoContainer key={id}>
+      <PhotoHeader>
+        <Avatar lg url={user.avatar} />
+        <Username>{user.username}</Username>
+      </PhotoHeader>
+      <PhotoFile src={file} />
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction>
+              <FontAwesomeIcon
+                style={{ color: isLiked ? "tomato" : "inherit" }}
+                icon={isLiked ? solidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </PhotoActions>
+        <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+      </PhotoData>
+    </PhotoContainer>
+  );
+}
+
+Photo.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+  }),
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.string.isRequired,
+  likes: PropTypes.string.isRequired,
+};
+
+export default Photo;
