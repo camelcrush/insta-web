449862150d45f20c7b84cba0f5d84b8ac295c075
camelcrush <dkxm0927@naver.com>
2022-05-07 13:01:28 +0900

    Parsing Hashtags: sanitizeHtml(), replace(), dangerouslySetInnerHTML

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,3 +1,4 @@
+import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
@@ -5,13 +6,32 @@ import { FatText } from "../shared";
 const CommentsContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Comment = ({ author, payload }) => {
+  // 필요한 html tag만 string을 통해 만들어 내기
+  const cleanedPayload = sanitizeHtml(
+    payload?.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
   return (
     <CommentsContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentsContainer>
   );
 };
