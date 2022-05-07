e7afabf3f091959372e4e0869e016199c4d2a336
camelcrush <dkxm0927@naver.com>
2022-05-07 16:22:17 +0900

    CreateComment 1

--- src/components/feed/Comments.js
+++ src/components/feed/Comments.js
@@ -1,14 +1,13 @@
+import { gql, useMutation } from "@apollo/client";
 import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
-import { FatText } from "../shared";
 import Comment from "./Comment";
 
 const CommentsContainer = styled.div`
   margin-top: 20px;
 `;
-const CommentCaption = styled.span`
-  margin-left: 10px;
-`;
+
 const CommentCount = styled.span`
   opacity: 0.7;
   margin: 10px 0px;
@@ -17,7 +16,41 @@ const CommentCount = styled.span`
   font-size: 10px;
 `;
 
-const Comments = ({ author, caption, commentCount, comments }) => {
+const PostCommentContainer = styled.div`
+  margin-top: 10px;
+  padding: 15px 0px 10px 0px;
+  border-top: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const PostCommentInput = styled.input`
+  width: 100%;
+  &::placeholder {
+    font-size: 12px;
+  }
+`;
+
+const CREATE_COMMENT_MUTATION = gql`
+  mutation createComment($photoId: Int!, $payload: String!) {
+    createComment(photoId: $photoId, payload: $payload) {
+      ok
+      error
+    }
+  }
+`;
+
+const Comments = ({ photoId, author, caption, commentCount, comments }) => {
+  const [createCommentMutation, { loading }] = useMutation(
+    CREATE_COMMENT_MUTATION
+  );
+  const { register, handleSubmit, setValue } = useForm();
+  const onValid = (data) => {
+    const { payload } = data;
+    if (loading) {
+      return;
+    }
+    createCommentMutation({ variables: { photoId, payload } });
+    setValue("payload", "");
+  };
   return (
     <CommentsContainer>
       <Comment author={author} payload={caption} />
@@ -31,11 +64,22 @@ const Comments = ({ author, caption, commentCount, comments }) => {
           payload={comment.payload}
         />
       ))}
+      <PostCommentContainer>
+        <form onSubmit={handleSubmit(onValid)}>
+          <PostCommentInput
+            {...register("payload", { required: true })}
+            name="payload"
+            type="text"
+            placeholder="Write a comment..."
+          />
+        </form>
+      </PostCommentContainer>
     </CommentsContainer>
   );
 };
 
 Comments.propTypes = {
+  photoId: PropTypes.number.isRequired,
   author: PropTypes.string.isRequired,
   caption: PropTypes.string,
   commentCount: PropTypes.number.isRequired,
